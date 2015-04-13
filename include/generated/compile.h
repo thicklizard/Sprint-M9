2b15c17da440b71939737e0b5107b434d13c616a
thicklizard <thicklizard@gmail.com>
2015-04-13 17:02:11 -0500

    tweak load factor once more

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#5 SMP PREEMPT Mon Apr 13 15:38:18 CDT 2015"
+#define UTS_VERSION "#6 SMP PREEMPT Mon Apr 13 16:59:29 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
