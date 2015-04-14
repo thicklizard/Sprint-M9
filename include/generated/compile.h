f548ef70fa0b28a6bcd3fcf4b588d63fac677c6e
thicklizard <thicklizard@gmail.com>
2015-04-14 15:03:17 -0500

    useless commit

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#11 SMP PREEMPT Tue Apr 14 13:51:59 CDT 2015"
+#define UTS_VERSION "#12 SMP PREEMPT Tue Apr 14 14:29:38 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.8.3 20140401 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2014.04 - Linaro GCC 4.8-2014.04) "
