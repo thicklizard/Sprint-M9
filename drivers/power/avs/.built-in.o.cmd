c39f2520e76be412bba58794064dc59a3b46877a
thicklizard <thicklizard@gmail.com>
2015-04-22 15:17:57 -0500

    simple_ondemands private data must be set to NULL

---
+++ drivers/power/avs/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/power/avs/built-in.o :=  rm -f drivers/power/avs/built-in.o; /home/thicklizard/android-toolchain-eabioptimized7/bin/aarch64-linux-gnu-ar rcsD drivers/power/avs/built-in.o
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 18 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#16 SMP PREEMPT Mon Apr 20 13:57:20 CDT 2015"
+#define UTS_VERSION "#18 SMP PREEMPT Wed Apr 22 14:15:26 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.8.3 20140401 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2014.04 - Linaro GCC 4.8-2014.04) "
