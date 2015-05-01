0a23c9367b4669c73b388771c75eec464435296f
thicklizard <thicklizard@gmail.com>
2015-05-01 15:59:29 -0500

    sched/idle: Avoid spurious wakeup IPIs

--- drivers/usb/misc/.built-in.o.cmd
+++ drivers/usb/misc/.built-in.o.cmd
@@ -1,1 +1,1 @@
-cmd_drivers/usb/misc/built-in.o :=  /home/thicklizard/android-toolchain-eabioptimized7/bin/aarch64-linux-gnu-ld -EL    -r -o drivers/usb/misc/built-in.o drivers/usb/misc/ehset.o 
+cmd_drivers/usb/misc/built-in.o :=  /home/thicklizard/android-toolchain-eabioptimized7/bin/aarch64-linux-gnu-ld -EL    -r -o drivers/usb/misc/built-in.o drivers/usb/misc/ehset.o drivers/usb/misc/trancevibrator.o 
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 23 */
+/* This file is auto generated, version 26 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#23 SMP PREEMPT Tue Apr 28 13:48:59 CDT 2015"
+#define UTS_VERSION "#26 SMP PREEMPT Fri May 1 15:57:33 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.8.3 20140401 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2014.04 - Linaro GCC 4.8-2014.04) "
