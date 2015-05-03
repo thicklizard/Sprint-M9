7de44cc05c60645d5946f411f297dba1aeb56f04
thicklizard <thicklizard@gmail.com>
2015-05-03 15:02:47 -0500

    arm64: add support for memtest

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 29 */
+/* This file is auto generated, version 30 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#29 SMP PREEMPT Sun May 3 11:02:01 CDT 2015"
+#define UTS_VERSION "#30 SMP PREEMPT Sun May 3 14:59:51 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.8.3 20140401 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2014.04 - Linaro GCC 4.8-2014.04) "
