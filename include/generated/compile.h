7f4c6cb2c21fa631fb986fe56bfd1dd2e0321f9f
thicklizard <thicklizard@gmail.com>
2015-05-18 17:26:58 -0500

    revert this

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#4 SMP PREEMPT Sat May 16 12:50:48 CDT 2015"
+#define UTS_VERSION "#5 SMP PREEMPT Mon May 18 13:24:32 CDT 2015"
 #define LINUX_COMPILE_BY "thicklizard"
 #define LINUX_COMPILE_HOST "BadseedCustoms"
 #define LINUX_COMPILER "gcc version 4.8.3 20140401 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2014.04 - Linaro GCC 4.8-2014.04) "
